c2b69bf34af538ee4b7fe2bae202250b6ee5c2a3
Kenny Hartwig <khartwig@sfu.ca>
2018-07-13 09:01:13 -0700

    Seed command added - 'npm run start:seed'

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(errorHandler);
 // start server
 // const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
 const port = config.development.port;
-models.sequelize.sync({force:true}).then(function() {
+models.sequelize.sync().then(function() {
     console.log('Models synchronized');
     app.listen(port, function() {
         console.log('Server listening on port ' + port);
