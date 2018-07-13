5036381f93048fded0bba2d212ef275cb762aa42
Kenny Hartwig <khartwig@sfu.ca>
2018-07-13 12:05:41 -0700

    Setter method for Bot so that the user won't overwrite the status

--- server/_infra/db/models/bot.js
+++ server/_infra/db/models/bot.js
@@ -6,7 +6,15 @@ module.exports = (sequelize, DataTypes) => {
     commandPrefix: DataTypes.STRING,
     status: { type: DataTypes.STRING, defaultValue: 'OFFLINE', allowNull: false }
     }, {
-        comment: "Users' Discord Bot instances that have a list of commands that can be invoked in a Discord guild"
+        comment: "Users' Discord Bot instances that have a list of commands that can be invoked in a Discord guild",
+        setterMethods: {
+            // Use to prevent user overwriting RO values like 'status'
+            writableFields(params){
+                if (params.name) this.setDataValue('name', params.name);
+                if (params.name) this.setDataValue('discordToken', params.discordToken);
+                if (params.name) this.setDataValue('commandPrefix', params.commandPrefix);
+            }
+        }
     });
   Bot.associate = function(models) {
     models.Bot.belongsTo(models.User, {
