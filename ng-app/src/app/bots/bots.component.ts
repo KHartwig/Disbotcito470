c6d2eb37136081946dac34288461dcf3f222aa2e
zha34 <zha34@sfu.ca>
2018-07-23 16:46:16 -0700

    try to fix start-stop problem in bots-list page

--- ng-app/src/app/bots/bots.component.ts
+++ ng-app/src/app/bots/bots.component.ts
@@ -29,50 +29,47 @@ export class BotsComponent implements OnInit {
   }
 
   loadBot(){
-          this.botService.getById(this.id).pipe(first()).subscribe(rcvdBot => {
+        this.botService.getById(this.id).pipe(first()).subscribe(rcvdBot => {
         this.bot = rcvdBot;
 
       });
   }
 
   startStop(i number) {
-    this.loadBot(i);
-    // this.loading = true;
-  
-    // this.botService.getById(i).pipe(first()).subscribe(rcvdBot => {
-    //   this.bot = rcvdBot;
-    // });
-  
-    // this.bot = ;
-    if ( this.bot.status == "ONLINE" ){
-      this.botService.stop(this.bot.id)
-      .pipe(first())
-      .subscribe(
-        data => {
-  
-  
-        },
-        error => {
-          this.alertService.error(error);
-      });
-    }
-  
-    else{
-      this.botService.start(this.bot.id)
-        .pipe(first())
-        .subscribe(
-          data => {
-  
-          console.log(this.selectedBot.status);
-  
-  
-  
-          },
-          error => {
-            this.alertService.error(error);
+        this.botService.getById(i).pipe(first()).subscribe(rcvdBot => {
+        this.bot = rcvdBot;
+
+
+          if ( this.bot.status == "ONLINE" ){
+            this.botService.stop(this.bot.id)
+            .pipe(first())
+            .subscribe(
+              data => {
+        
+        
+              },
+              error => {
+                this.alertService.error(error);
+            });
+          }
+        
+          else{
+            this.botService.start(this.bot.id)
+              .pipe(first())
+              .subscribe(
+                data => {
+        
+                console.log(this.selectedBot.status);
+        
+        
+        
+                },
+                error => {
+                  this.alertService.error(error);
+              });
+          }
+        
         });
-    }
-  
   
   }
 }
