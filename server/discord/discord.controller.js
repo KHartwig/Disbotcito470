7005115fde36b779fc56e559287a12f6405a0464
P <phillipf@sfu.ca>
2018-07-26 23:02:39 -0700

    Discord Controller and Router

--- server/discord/discord.controller.js
+++ server/discord/discord.controller.js
@@ -2,11 +2,57 @@ const discordService = require('./discord.service');
 
 module.exports = {
     start,
-    stop
+    stop.
+    getAllByBot,
+    attachGuildObject,
+    getById,
+    getMembersByGuildGid,
+    getEmojisByGid
 };
-
+//
 function start(req, res, next){
+  discordService.createClient(req.bot)
+    .then(() => res.json({}))
+    .catch(err => next(err));
+  botService.updateStatus(req.bot, 'ONLINE')
+    .then((bot) => res.json(bot))
+    .catch(err => next(err));
 }
 
 function stop(req, res, next) {
+  botService.updateStatus(req.bot, 'OFFLINE')
+    .then((bot) => res.json(bot))
+    .catch(err => next(err));
+  discordService.destroyClient(req.bot)
+    .then((bot) => res.json({}))
+    .catch(err => next(err));
+}
+
+function getAllByBot(req, res, next) {
+  discordService.getGuildById(req.bot)
+    .then((guild) => res.json(guild))
+    .catch(err => next(err));
+}
+
+function attachGuildObject(req, res, next) {
+  getGuildObject(req.bot)
+    .then(function (guild) {
+        if (!guild) res.sendStatus(404);
+        else {
+            req.guild= guild;
+            next();
+        }
+    })
+    .catch(err => next(err));
+}
+function getById(req, res, next) {
+  discordService.getGuildById(req.guild)
+}
+
+function getMembersByGuildGid(req, res, next) {
+  discordService.getGuildMembers(req.guild)
+}
+
+function getEmojisByGid(req, res, next) {
+  discordService.getGuildEmojis(req.guild)
 }
--- server/discord/discord.router.js
+++ server/discord/discord.router.js
@@ -3,5 +3,14 @@ const router = express.Router();
 const discordController = require('./bots.controller');
 
 router.put('/start', discordController.start);
+router.put('/stop', discordController.stop);
+
+router.get('/guilds/', discordController.getAllByBot);
+router.get('/guilds/:gid', discordController.getById);
+router.get('/guilds/:gid/member'), discordController.getMembersByGid);
+router.get('/guilds/:gid/emojis'), discordController.getEmojisByGid);
+
+//middleware
+router.use('/guilds/', discordController.attachGuildObject);
 
 module.exports = router;
